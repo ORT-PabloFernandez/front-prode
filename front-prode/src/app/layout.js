3775941511d5d11a0ce9bbccd37ab1585534e42c
Enrique Wainstein <enriquewainsteinpellegrini@gmail.com>
2026-05-20 20:56:27 -0300

    agrego components/header

--- front-prode/src/app/layout.js
+++ front-prode/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
+import Header from "./Components/Header/Header.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,8 +14,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" className={`${geistSans.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+
+    
+    <html
+  lang="es"
+  className={`${geistSans.variable} h-full antialiased`}
+>
+  <body className="min-h-full flex flex-col">
+    <Header />
+
+    <main className="flex-1">
+      {children}
+    </main>
+  </body>
+</html>
+
+      
+
+   
   );
 }
